865f8f58d546e13dfe94b991a0fcedbed2989675
Yeasin7772 <yeasin26yeasin26@gmail.com>
2023-11-04 23:46:30 +0600

    Added a banner section to the homepage with a title and description.

--- src/Shared/NavBar.jsx
+++ src/Shared/NavBar.jsx
@@ -16,39 +16,59 @@ const NavBar = () => {
         <NavLink to='/myFoodRequest' className={({ isActive, isPending }) =>
             isActive ? 'btn btn-primary btn-sm' : isPending ? 'btn btn-ghost  btn-sm' : ' '
         }>My Food Request</NavLink>
-        
+
 
     </>
     return (
         <div className="drawer">
-        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
-        <div className="drawer-content flex flex-col">
-          {/* Navbar */}
-          <div className="w-full navbar bg-base-300">
-            <div className="flex-none lg:hidden">
-              <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-              </label>
-            </div> 
-            <div className="flex-1 px-2 mx-2">Navbar Title</div>
-            <div className="flex-none hidden lg:block">
-              <ul className="menu menu-horizontal">
-                {/* Navbar menu content here */}
-               {navLinks}
-              </ul>
+            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col">
+                {/* Navbar */}
+                <div className="w-full navbar bg-base-300">
+                    <div className="flex-none lg:hidden">
+                        <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                        </label>
+                    </div>
+                    <div className="flex-1 px-2 mx-2 text-5xl font-bold text-yellow-500">Be AHand </div>
+                    <div className="flex-none hidden lg:block">
+                        <div className="flex justify-center items-center gap-5 ">
+                            {/* Navbar menu content here */}
+                            {navLinks}
+
+                        </div>
+
+
+                    </div>
+                    <div className="dropdown ml-5 dropdown-end">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                            </div>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li>
+                                <a className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </a>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
             </div>
-          </div>
-          {/* Page content here */}
-          Content
-        </div> 
-        <div className="drawer-side">
-          <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label> 
-          <ul className="menu p-4 w-80 min-h-full bg-base-200">
-            {/* Sidebar content here */}
-           {navLinks}
-          </ul>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+                <ul className="menu p-4 w-80 min-h-full bg-base-200">
+                    {/* Sidebar content here */}
+                    {navLinks}
+                </ul>
+            </div>
+
         </div>
-      </div>
     );
 };
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+
+const Header = () => {
+    return (
+        <div className="hero min-h-full" style={{ backgroundImage: 'url(http://unlockdesizn.com/html/non-profit/be-ahand/demo/images/about/1.jpg)' }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className=" px-12">
+                    <h1 className="mb-5 text-3xl font-bold">JOIN WITH US, <br /> YOUR ATTENTION IS CHANGED THE PART OF WORLD.</h1>
+                    <p className="mb-5"><small>Cupiditate qui molestias fugit voluptatibus laudantium maxime <br />voluptate corrupti ab repudiandae dolor repellendus? laudantium maxime voluptate corrupti ab repudiandae dolor repellendus?</small></p>
+                    <div className="flex justify-center gap-6">
+                        <button className="btn btn-outline btn-primary">Donation Now</button>
+
+                        <button className="btn btn-outline btn-warning">Join With us</button>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
