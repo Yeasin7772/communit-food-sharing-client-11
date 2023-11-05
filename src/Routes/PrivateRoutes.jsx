1377f5d52bb3d0c23f15f368d6ab3be03707e3a9
Yeasin7772 <yeasin26yeasin26@gmail.com>
2023-11-05 14:46:11 +0600

    use lottie animation for loading

--- src/Routes/PrivateRoutes.jsx
+++ src/Routes/PrivateRoutes.jsx
@@ -1,10 +1,18 @@
+import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import animations from '../../public/animations.json'
+import Lottie from "lottie-react";
+const PrivateRoutes = ({ children }) => {
+    const { user, loading } = useAuth()
 
-const PrivateRoutes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    if (loading) {
+        return <div className="flex justify-center items-center"><Lottie animationData={animations}> </Lottie></div>
+    }
+    if (user) {
+        return children
+    }
+
+    return <Navigate state={location.pathname} to='/login'></Navigate>
 };
 
 export default PrivateRoutes;
